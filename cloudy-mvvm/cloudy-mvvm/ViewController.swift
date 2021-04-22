f453e99b00d99e2a6b2259a9c2b922569b183f60
Ygor Nascimento <ygor.nascimento@outlook.com>
2021-04-22 12:24:56 -0300

    Assets Added

---
+++ cloudy-mvvm/cloudy-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cloudy-mvvm
+//
+//  Created by Ygor Nascimento on 22/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
